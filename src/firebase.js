0f0b3f3f39304f44183370a9da20069c65e9aa5d
Chungywungy <placeholder0254@gmail.com>
2025-04-13 03:36:30 -0700

    private database, anon logins

--- src/firebase.js
+++ src/firebase.js
@@ -2,11 +2,17 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore"; 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth, signInAnonymously } from "firebase/auth";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const auth = getAuth();
+signInAnonymously(auth)
+  .then(() => {
+    console.log("Signed in anonymously ✅");
+  })
+  .catch((error) => {
+    console.error("Anonymous sign-in failed:", error);
+  });
+  
 const firebaseConfig = {
   apiKey: "AIzaSyDt3jalGC3bPfmee4mcaIV8jWlh1yWQw04",
   authDomain: "movieratings-c1445.firebaseapp.com",
